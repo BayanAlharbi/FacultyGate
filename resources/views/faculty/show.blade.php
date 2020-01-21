244100b04a44162977f4f8a741423377060a102b
BayanAlharbi <Alharbi.bayan1@gmail.com>
2020-01-21 10:14:15 +0300

    fixed: faculty show blade

--- resources/views/faculty/show.blade.php
+++ resources/views/faculty/show.blade.php
@@ -14,28 +14,25 @@
                 <div class="card-header">{{ __('Faculty Member') }}</div>
                 <div class="card-body">
                     <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">English Name: {!! $faculty->english_name !!}</h6>
-                    </div>
-                    <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">Arabic Name: {!! $faculty->arabic_name !!}</h6>
-                    </div>
-                    <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">Badge: {!!$faculty->badge !!}</h6>
-                    </div>
-                    <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">Academic Rank: {!!$faculty->academic_rank !!}</h6>
-                    </div>
-                    <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">Admin Position: {!!$faculty->admin_position !!}</h6>
-                    </div>
-                    <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">Joining Date: {!!$faculty->joining_date !!}</h6>
-                    </div>
-                    <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">Promotion Date: {!!$faculty->promotion_date !!}</h6>
-                    </div>
-                    <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">Last Working Date: {!!$faculty->last_working_date !!}</h6>
+                        <dl class="row">
+                            <dt class="col-sm-3">English Name:</dt>
+                            <dd class="col-sm-9"> {!! $faculty->english_name !!}</dd>
+                            <dt class="col-sm-3">Arabic Name:</dt>
+                            <dd class="col-sm-9">{!! $faculty->arabic_name !!}</dd>
+                            <dt class="col-sm-3">Badge:</dt>
+                            <dd class="col-sm-9">{!!$faculty->badge !!}</dd>
+                            <dt class="col-sm-3">Academic Rank:</dt>
+                            <dd class="col-sm-9">{!!$faculty->academic_rank !!}</dd>
+                            <dt class="col-sm-3">Admin Position:</dt>
+                            <dd class="col-sm-9"> {!!$faculty->admin_position !!}</dd>
+                            <dt class="col-sm-3">Joining Date:</dt>
+                            <dd class="col-sm-9"> {!!$faculty->joining_date !!}</dd>
+                            <dt class="col-sm-3">Promotion Date:</dt>
+                            <dd class="col-sm-9"> {!!$faculty->promotion_date !!}</dd>
+                            <dt class="col-sm-3">Last Working Date:</dt>
+                            <dd class="col-sm-9"> {!!$faculty->last_working_date !!}</dd>
+
+                        </dl>
                     </div>
                 </div>
             </div>
